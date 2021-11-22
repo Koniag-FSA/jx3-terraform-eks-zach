df49da7a48f0581f9e6067fcc9fe0d773499955e
Zach Curtis <zachariahtimothy@gmail.com>
2021-11-22 11:26:14 -0500

    Downgrade k8s version

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "profile" {
 variable "cluster_version" {
   description = "Kubernetes version to use for the EKS cluster."
   type        = string
-  default     = "1.21"
+  default     = "1.20"
 }
 
 variable "vault_user" {
